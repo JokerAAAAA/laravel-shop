ae247d709bc1ce641a0de136da133faebbd4f590
董坤鸿 <249529979@qq.com>
2020-06-18 10:23:52 +0800

    管理后台 - 用户列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,10 +5,11 @@ namespace App\Models;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Encore\Admin\Traits\DefaultDatetimeFormat;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable, DefaultDatetimeFormat;
 
     /**
      * 可以被批量赋值的属性
