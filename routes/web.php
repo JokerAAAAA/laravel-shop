f47d053d9250f238ecc4f68db6375d4fd534cb90
董坤鸿 <249529979@qq.com>
2020-07-03 11:27:10 +0800

    用户界面 - 订单详情页

--- routes/web.php
+++ routes/web.php
@@ -40,10 +40,12 @@ Route::group(
         // 移除购物车
         Route::delete('carts/{sku}', 'CartsController@destroy')->name('carts.destroy');
 
-        //订单列表
+        // 订单列表
         Route::get('orders', 'OrdersController@index')->name('orders.index');
-        //创建订单
+        // 创建订单
         Route::post('orders', 'OrdersController@store')->name('orders.store');
+        // 订单详情
+        Route::get('orders/{order}', 'OrdersController@show')->name('orders.show');
     }
 );
 
