d77e1c0c3d89d0e8880e852682ddf0046c9f136c
董坤鸿 <249529979@qq.com>
2020-07-08 17:58:25 +0800

    管理后台 - 订单列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -4,16 +4,21 @@ use Illuminate\Routing\Router;
 
 Admin::routes();
 
-Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
-    'as'            => config('admin.route.prefix') . '.',
-], function (Router $router) {
+Route::group(
+    [
+        'prefix' => config('admin.route.prefix'),
+        'namespace' => config('admin.route.namespace'),
+        'middleware' => config('admin.route.middleware'),
+        'as' => config('admin.route.prefix').'.',
+    ],
+    function (Router $router) {
 
-    $router->get('/', 'HomeController@index')->name('home');
-    // 用户管理
-    $router->get('users', 'UsersController@index');
-    // 商品管理
-    $router->resource('products', 'ProductsController');
-});
+        $router->get('/', 'HomeController@index')->name('home');
+        // 用户管理
+        $router->get('users', 'UsersController@index')->name('admin.users.index');
+        // 商品管理
+        $router->resource('products', 'ProductsController');
+        // 订单管理
+        $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+    }
+);
