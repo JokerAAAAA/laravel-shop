88d5384cd49a0ac7cd00a0efb0e2edb045a608c6
董坤鸿 <249529979@qq.com>
2020-07-09 15:26:23 +0800

    管理后台 - 订单详情

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -15,10 +15,12 @@ Route::group(
 
         $router->get('/', 'HomeController@index')->name('home');
         // 用户管理
-        $router->get('users', 'UsersController@index')->name('admin.users.index');
+        $router->get('users', 'UsersController@index')->name('users.index');
         // 商品管理
         $router->resource('products', 'ProductsController');
-        // 订单管理
-        $router->get('orders', 'OrdersController@index')->name('admin.orders.index');
+        // 订单管理-订单列表
+        $router->get('orders', 'OrdersController@index')->name('orders.index');
+        // 订单管理-订单详情
+        $router->get('orders/{order}', 'OrdersController@show')->name('orders.show');
     }
 );
