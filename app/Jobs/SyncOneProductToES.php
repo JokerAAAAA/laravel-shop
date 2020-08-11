15306a4d0b0b0ddeba2b0f19f334cefa6c6b4f31
董坤鸿 <249529979@qq.com>
2020-08-11 13:20:41 +0800

    同步商品数据至 Elasticsearch

---
+++ app/Jobs/SyncOneProductToES.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Product;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+/**
+ * ShouldQueue 代表异步
+ *
+ * Class SyncOneProductToES
+ * @package App\Jobs
+ */
+class SyncOneProductToES implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $product;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param Product $product
+     * @return void
+     */
+    public function __construct(Product $product)
+    {
+        $this->product = $product;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $data = $this->product->toESArray();
+        app('es')->index(
+            [
+                'index' => 'products',
+                'id' => $data['id'],
+                'body' => $data,
+            ]
+        );
+    }
+}
