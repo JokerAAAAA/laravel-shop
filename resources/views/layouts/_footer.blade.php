80a84d1b1d155eba847ae8ffa15af2ebf5e35fb1
董坤鸿 <249529979@qq.com>
2020-09-24 17:04:58 +0800

    数据库活动表新增字段

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="float-left">
-            由 <a href="https://buqiu.wang" target="_blank">Ken</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            由 <a href="https://buqiu.wang" target="_blank">董坤鸿</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="float-right"><a href="mailto:249529979@qq.com">联系我们</a></p>
